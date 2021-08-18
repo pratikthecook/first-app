3571b84d1f0f590a48a2a9088b049ca502b9389f
Pratik <pratik@Pratiks-MacBook-Pro.local>
2021-08-18 17:42:03 +0530

    Initial commit

---
+++ src/test/java/com/first/app/first/app/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.app.first.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
